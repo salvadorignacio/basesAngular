b93658d94203b38d3b09ce43517a4d5d4e94c0a7
salvadorignacio <salvadorignacio23@gmail.com>
2022-05-12 20:53:53 -0400

    Modifica Componente Listado

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -5,5 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './listado.component.html',
 })
 export class ListadoComponent {
+  heroes: string[] = ['Spiderman', 'Ironman', 'Allmigth', 'Lemillion'];
+  heroesEliminados: string[] = [];
+  borrarHeroe (){
+    const heroeBorrado: string = this.heroes.shift() || '';
+    if (heroeBorrado != '') {
+      this.heroesEliminados.push(heroeBorrado);
+    }
 
+
+    console.log(this.heroesEliminados);
+  }
 }
+
